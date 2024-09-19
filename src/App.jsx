391b9e65624a7211b9a87ef7c76ea0f499f739ad
vaishnavi-sawant <vaishnavisawant310@gmail.com>
2024-09-19 16:34:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import GithubSearch from './components/GithubSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubSearch />
+    </div>
+  );
+}
+
+export default App;
